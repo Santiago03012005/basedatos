734c5414f88a48fd89bbbe4745eb7749d41d4e83
Santiago03012005 <132488385+Santiago03012005@users.noreply.github.com>
2023-06-26 12:10:48 -0500

    Add files via upload

---
+++ FORMULARIOWEB/src/main.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdPeNIovI5YULUB_GJgI8dkqtNo_C7U5Q",
+    authDomain: "registroweb-4f81c.firebaseapp.com",
+    projectId: "registroweb-4f81c",
+    storageBucket: "registroweb-4f81c.appspot.com",
+    messagingSenderId: "244996046521",
+    appId: "1:244996046521:web:fd0e5c8fabb95a2226e568"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+const btnAceptarRegistrate = document.getElementById(btnAceptarRegistrate)
+
+btnAceptarRegistrate.addEventListener('click' () => {
+    let emailRegistrate = document.getElementsById('emailRegistrate')
+    let passwordRegistrate = document.getElementsById('passwordRegistrate')
+})
